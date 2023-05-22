f181ba4825e033173610d4188c2feaa838272e54
cvillamer13 <cvillamer13@gmail.com>
2023-05-22 10:22:45 +0800

    Login form

--- server.js
+++ server.js
@@ -30,8 +30,23 @@ app.post('/users', async (req, res) => {
 });
 
 
-app.post('/users/login/', async (res, req) => {
+app.post('/users/login', async (req, res) => {
+    
     const user = users.find(user => user.name === req.body.name);
+    console.log(users);
+    if(user == null){
+        return res.status(400).send('Cannot find user');
+    }
+
+    try{
+       if(await bcrypt.compare(req.body.password, user.password)){
+            res.send('Success');
+       }else{
+        res.send('Failed');
+       }
+    } catch{
+        res.status(500).send();
+    }
 });
 
 app.listen(3000);
